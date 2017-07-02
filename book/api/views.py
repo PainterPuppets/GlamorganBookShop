1942ca34b683d4d23b96762aa8bca414d910b206
painterpuppets <13893791616@163.com>
2017-07-02 23:35:15 +0800

    remove ignore file

--- book/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from book.models import Book
-from book.api.serializers import BookSerializer
-
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
--- order/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from order.models import Order
-from order.api.serializers import OrderSerializer
-
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
